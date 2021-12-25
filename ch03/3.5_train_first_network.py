5fe523a228c39fe52a81408e7c26f3fa3d0820f2
consulterai <consulterai@139.com>
2021-12-25 23:21:32 +0800

    ch03

--- ch03/3.5_train_first_network.py
+++ ch03/3.5_train_first_network.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torchvision
 from torch.nn import CrossEntropyLoss
-from torch.optim import Adam
+from torch.optim import SGD
 from torch.utils.data import DataLoader
 
 
@@ -24,18 +24,26 @@ class MLP(nn.Module):
         return out
 
 
-def case_infer():
-    input = torch.rand([5, 1, 28, 28])
+transform = torchvision.transforms.Compose(
+    [
+        torchvision.transforms.RandomHorizontalFlip(0.5),
+        torchvision.transforms.RandomAffine(0, translate=(0.1, 0.1)),
+        torchvision.transforms.RandomRotation((-5, 5)),
+        torchvision.transforms.ColorJitter(brightness=0.3,
+                                           contrast=0.3),
+        torchvision.transforms.CenterCrop(
+            (int(28 * 0.8), int(28 * 0.8))),
+        torchvision.transforms.Resize((28, 28)),
+        torchvision.transforms.ToTensor(),
+    ]
+)
+
+
+def train():
     model = MLP()
     model.eval()
-
-    out = model(input)
-    print(out.shape)
-
-
-def case_train():
-    model = MLP()
-    model.eval()
+    if torch.cuda.is_available():
+        model = torch.nn.DataParallel(model)
 
     train_dataset = torchvision.datasets.MNIST(root="./",
                                                download=True,
@@ -51,40 +59,64 @@ def case_train():
     train_loader = DataLoader(train_dataset, batch_size=8,
                               shuffle=True, num_workers=0,
                               drop_last=True)
+    test_loader = DataLoader(test_dataset, batch_size=8,
+                             shuffle=True, num_workers=0,
+                             drop_last=True)
     criteria = CrossEntropyLoss()
-    optimizer = Adam(model.parameters(), lr=1e-4)
+    optimizer = SGD(model.parameters(), lr=1e-2,
+                    momentum=0.9, weight_decay=1e-4)
 
+    def lr_step_func(epoch):
+        return 1.0 if epoch < 1 else 0.1 ** len(
+            [m for m in [10, 15, 18] if m - 1 <= epoch])
+
+    scheduler_lr = torch.optim.lr_scheduler.LambdaLR(
+        optimizer=optimizer, lr_lambda=lr_step_func)
+
+    whole_train_steps = len(train_loader)
     for epoch in range(20):
         model.train()
         Loss_Sum = 0.0
+        print("epoch：{} using lr :{} ".format(epoch,
+                                              optimizer.param_groups
+                                              [0]['lr']))
         for cnt, (imgs, labels) in enumerate(train_loader):
+            if torch.cuda.is_available():
+                imgs = imgs.cuda()
+                labels = labels.cuda()
             out = model(imgs)
             loss = criteria(out, labels)
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
             Loss_Sum += float(loss)
-            print("epoch: {} [{}/{}] "
-                  "loss:{} Acc:{}".format(epoch, cnt,
-                                          len(train_loader),
-                                          float(loss),
-                                          float((torch.argmax(
-                                              out, dim=1)
-                                                 == labels).sum())
-                                          / float(imgs.shape[0])))
-        print("epoch：{} Loss_Sum:{}".format(epoch, Loss_Sum
-                                            / len(train_loader)))
+
+            # print("epoch: {} [{}/{}] "
+            #       "loss:{} Acc:{}".format(epoch, cnt,
+            #                               len(train_loader),
+            #                               float(loss),
+            #                               float((torch.argmax(
+            #                                   out, dim=1)
+            #                                      == labels).sum())
+            #                               / float(imgs.shape[0])))
+        print("epoch：{} Loss_Sum:{} lr:{}".format(epoch, Loss_Sum
+                                                  / whole_train_steps,
+                                                  scheduler_lr.get_lr()
+                                                  ))
+        scheduler_lr.step()
         model.eval()
         AllCnt = 0.0
         RightCnt = 0.0
-        for cnt, (imgs, labels) in enumerate(test_dataset):
+        for cnt, (imgs, labels) in enumerate(test_loader):
+            if torch.cuda.is_available():
+                imgs = imgs.cuda()
+                labels = labels.cuda()
             out = model(imgs)
             AllCnt += imgs.shape[0]
             RightCnt += (torch.argmax(out, dim=1) == labels).sum()
         print("epoch：{} TestAcc:{}".format(epoch, RightCnt / AllCnt))
-        torch.save(model.state_dict(), "epoch_{}.pth".format(epoch))
+    torch.save(model.state_dict(), "epoch_{}.pth".format(epoch))
 
 
 if __name__ == '__main__':
-    # case_infer()
-    case_train()
+    train()
